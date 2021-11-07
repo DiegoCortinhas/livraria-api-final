d143b05d20fff09caf95a4a673e8bf97320a88b4
Diego Pereira Cortinhas <diego.cortinhas@gmail.com>
2021-11-07 11:03:57 -0300

    Mapeamento das Entidades CadastroAutor e CadastroLivro

--- livraria-api/src/main/java/br/com/alura/livraria/modelo/CadastroLivros.java
+++ livraria-api/src/main/java/br/com/alura/livraria/modelo/CadastroLivros.java
@@ -2,6 +2,13 @@ package br.com.alura.livraria.modelo;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import br.com.alura.livraria.dto.CadastroAutoresDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,12 +21,19 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="livros")
 public class CadastroLivros {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
 	private String titulo;
 	private LocalDate dataLancamento;
-	private int numeroPaginas;
-	private CadastroAutoresDto autor;
+	private Integer numeroPaginas;
+	
+	@ManyToOne 
+	private CadastroAutores cadastroAutor;
 	
 	
 }
